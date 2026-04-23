c71d6f03d3a542b52cf3bf27f58ce10c7a296854
lin_shuan <linshuan@zuzuche.com>
2026-04-23 11:29:56 +0800

    v2.0.2: 支持多种module.scss的写法

--- example/src/scss-example/js/scss-example.js
+++ example/src/scss-example/js/scss-example.js
@@ -12,9 +12,9 @@ class App extends Component {
             <div>
                 <h1 className={'mmd'}>OMG-CLI</h1>
                 <Button>111143334</Button>
-                <p className={styles['md-77']}>this is text</p>
+                <p className={styles['md-77']}>this is text11</p>
                 <p className={styles.className1}>this is text</p>
-                <p className={styles.md77}>this is text</p>
+                <p className={styles.md77}>this is text22</p>
                
             </div>
         )
